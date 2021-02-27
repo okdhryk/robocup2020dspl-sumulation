fbda9f71fece5a1675280af57af78370d634fe15
Hiroyuki Okada's GitHub <h.okada@eng.tamagawa.ac.jp>
2021-02-27 14:37:55 +0900

    Update latexmk.sh

--- latexmk.sh
+++ latexmk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export PATH=$PATH:/usr/local/texlive/2020/bin/x86_64-linuxmusl
-latexmk -f --pdf --quiet -synctex=1 -output-directory=./.workspace/ Rulebook.tex
+latexmk -f --pdf --quiet -synctex=1 -output-directory=./work/ Rulebook.tex
